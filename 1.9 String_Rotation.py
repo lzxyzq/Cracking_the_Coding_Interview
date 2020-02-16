f573cc6d39df6c20b2beb272ffece94928c140fd
Dandy121 <lzxyzq@gmail.com>
2020-02-16 13:35:25 -0500

    Cracking the Coding Interview

---
+++ 1.9 String_Rotation.py
@@ -0,0 +1,2 @@
+# Assume you have a method is Substring which check if one word is substring of another. Given two strings,s1 and s2, write code to check if s2 is a roration of s1 using only one call to isSubtring(e.g."waterbottle" is a rotation of "erbottlewat").
+
